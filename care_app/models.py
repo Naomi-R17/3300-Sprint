a4c0a79bf0e84dc3eb98a0fa1da535b4455803ec
Naomi-R17 <112025982+Naomi-R17@users.noreply.github.com>
2024-04-05 13:31:36 -0600

    patient model pt 1

--- care_app/models.py
+++ care_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Patient(models.Model):
+    # starting off with very basic information, all required
+    name = models.CharField(max_length=200, blank=False)
+    email = models.CharField("Email", max_length=200, blank=False)
+    phone = models.CharField("Phone Number", max_length=200, blank=False)
+    dob = models.CharField("Date of Birth", max_length=200, blank=False)
