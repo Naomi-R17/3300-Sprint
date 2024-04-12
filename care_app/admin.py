d63faf80bfe2e0c0c3a9393b6b9d22b3eaf9c922
Naomi-R17 <112025982+Naomi-R17@users.noreply.github.com>
2024-04-11 18:56:52 -0600

    Medications model and Home page color/logo

--- care_app/admin.py
+++ care_app/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Patient
+from .models import Patient, Medication
 
 admin.site.register(Patient)
+admin.site.register(Medication)
--- care_app/models.py
+++ care_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 
 
+
 # Create your models here.
 class Patient(models.Model):
     name = models.CharField(max_length=200, blank=False)
@@ -16,5 +17,77 @@ class Patient(models.Model):
 
     def get_absolute_url(self):
         return reverse('patient-detail', args=[str(self.id)])
+
+
+# Medication list
+class Medication(models.Model):
+    MEDICATION_OPTIONS = (
+        ('Acetaminophen', 'Acetaminophen'),
+        ('Adderall', 'Adderall'),
+        ('Amitriptyline', 'Amitriptyline'),
+        ('Amlodipine', 'Amlodipine'),
+        ('Amoxicillin', 'Amoxicillin'),
+        ('Ativan', 'Ativan'),
+        ('Atorvastatin', 'Atorvastatin'),
+        ('Azithromycin', 'Azithromycin'),
+        ('Benzonatate', 'Benzonatate'),
+        ('Brilinta', 'Brilinta'),
+        ('Bunavail', 'Bunavail'),
+        ('Buprenophine', 'Buprenophine'),
+        ('Cephalexin', 'Cephalexin'),
+        ('Ciprofloxacin', 'Ciprofloxacin'),
+        ('Citalopram', 'Citalopram'),
+        ('Clindamycin', 'Clindamycin'),
+        ('Clonazepam', 'Clonazepam'),
+        ('Cyclobenzaprine', 'Cyclobenzaprine'),
+        ('Cymbalta', 'Cymbalta'),
+        ('Doxycycline', 'Doxycycline'),
+        ('Dupixent', 'Dupixent'),
+        ('Entresto', 'Entresto'),
+        ('Entyvio', 'Entyvio'),
+        ('Farxiga', 'Farxiga'),
+        ('Fentaynl', 'Fentanyl'),
+        ('Gabapentin', 'Gabapentin'),
+        ('Gilenya', 'Gilenya'),
+        ('Humira', 'Humira'),
+        ('Hydrochlorothiazide', 'Hydrochlorothiazide'),
+        ('Ibuprofen', 'Ibuprofen'),
+        ('Imbruvica', 'Imbruvica'),
+        ('Januvia', 'Januvia'),
+        ('Kevzara', 'Kevzara'),
+        ('Leqvio', 'Leqvio'),
+        ('Lexapro', 'Lexapro'),
+        ('Lisinopril', 'Lisinopril'),
+        ('Lyrica', 'Lyrica'),
+        ('Melatonin', 'Melatonin'),
+        ('Meloxicam', 'Meloxicam'),
+        ('Metformin', 'Metformin'),
+        ('Methadone', 'Methadone'),
+        ('Metoprolol', 'Metoprolol'),
+        ('Naloxone', 'Naloxone'),
+        ('Nurtec', 'Nurtec'),
+        ('Omeprazole', 'Omeprazole'),
+        ('Ozempic', 'Ozempic'),
+        ('Pantoprazole', 'Pantoprazole'),
+        ('Prednisone', 'Prednisone'),
+        ('Tramadol', 'Tramadol'),
+        ('Trazodone', 'Trazodone'),
+        ('Viagra', 'Viagra'),
+        ('Wellbutrin', 'Wellbutrin'),
+        ('Xanax', 'Xanax')
+    )
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    name = models.CharField("Medication Name", max_length=200, choices=MEDICATION_OPTIONS, blank=False)
+    dosage = models.IntegerField("Dosage", blank=False)
+    takenDaily = models.BooleanField("Taken Daily?", default=False)
+    startDate = models.DateField("Starting Date", blank=False, null=False)
+    endDate = models.DateField("Ending Date", blank=False, null=False)
+
+    class Meta:
+        verbose_name = "Medication"
+        verbose_name_plural = "Medications"
+
+    def __str__(self):
+        return f"{self.patient.name}'s {self.name}"
 
 
